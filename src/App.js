a1e9a10568ed79ee7c1094e22a4b400d648463e2
fespeche <florencia.espeche@wnco.com>
2019-09-19 10:25:39 -0500

    fix errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './components/Login'
+//import Login from './components/Login'
 import './App.css';
 import Button from './components/Button'
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import phoneCall from
-    "/Users/matiasflores/hackaton/webapp-client/src/Images/phone-call.png"
-import speaker from "/Users/matiasflores/hackaton/webapp-client/src/Images/speaker.png"
+import phoneCall from '../Images/phone.png'
+
+import speaker from '../Images/megaphone.png'
+
 import "../Button.css"
 import axios from 'axios';
 class Button extends React.Component {
@@ -17,6 +18,7 @@ class Button extends React.Component {
                 <div onClick={this.handleEmergencia}>
                     <img src={phoneCall} />
                 </div>
+                <br/>
                 <div onClick={this.handleDenuncia}>
                     <img src={speaker} />
                 </div>
