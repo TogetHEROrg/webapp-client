fb9ce87cd32bdbb088464f279cd7393b9df0db3b
fespeche <florencia.espeche@wnco.com>
2019-09-25 10:29:25 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import Form from './components/FormContainer'
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+
+//Pages
+import Form from './components/FormContainer'
 import Button from './components/Button'
 
-function App() {
-  return (
-    <div className="container">
-        <Form/> 
-    </div>
+const App = () => {
+  return <Router>
+    <Route path="/" exact component={Button}/>
+    <Route path="/login" exact component={Form}/>
+  </Router>
+
       
-  );
+
 }
 
 export default App
