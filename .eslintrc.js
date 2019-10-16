4ff06a8d6178cf2c5d4288ca65b82d446c95e0ea
Ata Sanchez <atasanchez@Atas-MacBook-Air.local>
2019-10-15 22:46:38 -0300

    Add call to phone

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+    'jsx-a11y/anchor-has-content': 0,
   },
   settings: {
     react: {
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,21 +1,65 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 import './styles.css';
 
 // Firebase
+import firebase from 'firebase';
 import { providers, firebaseAppAuth } from '../../services/auth';
 import withFirebaseAuth from 'react-with-firebase-auth';
+import M from 'materialize-css';
 
 // Assets
 import speakerImage from '../../assets/megaphone_img.png';
 import phoneImage from '../../assets/phone_img.png';
 
 const Dashboard = ({ signOut }) => {
+  const aEl = useRef(null);
+  const db = firebase.firestore();
+
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+
   const handleEmergency = () => {
-    console.log('Press!');
+    db.collection('reclamos')
+      .add({
+        date: new Date(),
+        description: 'Contaminación Ambiental',
+        tipo: 'contaminacion_ambiental',
+      })
+      .then(() => {
+        M.toast({
+          html:
+            '<span>Reclamo enviado!</span><button class="btn-flat toast-action">👍</button>',
+        });
+      })
+      .catch(() => {
+        M.toast({
+          html:
+            '<span>Algo salio mal...</span><button class="btn-flat toast-action">👎</button>',
+        });
+      });
   };
 
   const handleReport = () => {
-    console.log('Press!');
+    aEl.current.click();
+    db.collection('reclamos')
+      .add({
+        date: new Date(),
+        description: 'Violencia de Genero',
+        tipo: 'violencia_genero',
+      })
+      .then(() => {
+        M.toast({
+          html:
+            '<span>La ayuda va en camino!</span><button class="btn-flat toast-action">👮‍</button>',
+        });
+      })
+      .catch(() => {
+        M.toast({
+          html:
+            '<span>Algo salio mal...</span><button class="btn-flat toast-action">👎</button>',
+        });
+      });
   };
 
   const handleSignOut = () => {
@@ -31,6 +75,8 @@ const Dashboard = ({ signOut }) => {
       <nav>
         <div className="text">Together App</div>
       </nav>
+
+      <a href="tel:144" className="phone" ref={aEl} />
 
       <div className="container">
         <button className="btn buttons-emergencia" onClick={handleEmergency}>
